cd44b18bfc4de41748615277a547109b6edd2fcf
Ivan Diesel <dieselpardal@gmail.com>
2017-01-11 10:44:10 -0200

    First Version

--- test/test_controller.py
+++ test/test_controller.py
@@ -9,22 +9,22 @@ class AddTest(unittest.TestCase):
         pass
 
     def test_loop_1_6(self):
-        '''Verify that 2 zeros with 5 is 05'''
+        '''Verify that size e day with 1 and 6 is 3'''
         hir = hotel_infinite_rooms.HotelInfiniteRooms()
         self.assertEqual(hir.membres_day(1, 6), 3)
 
     def test_loop_3_10(self):
-        '''Verify that 2 zeros with 5 is 05'''
+        '''Verify that size e day with 3 and 10 is 5'''
         hir = hotel_infinite_rooms.HotelInfiniteRooms()
         self.assertEqual(hir.membres_day(3, 10), 5)
 
     def test_loop_3_14(self):
-        '''Verify that 2 zeros with 5 is 05'''
+        '''Verify that size e day with 3 and 14 is 6'''
         hir = hotel_infinite_rooms.HotelInfiniteRooms()
         self.assertEqual(hir.membres_day(3, 14), 6)
 
     def test_recrusive_1_6(self):
-        '''Verify that 2 zeros with 5 is 0005'''
+        '''Verify that size e day with 1 and 6 is 3 with recrusive'''
         hir = hotel_infinite_rooms.HotelInfiniteRooms()
         self.assertEqual(hir.membres_day_res(1, 6, 1), 3)
 
